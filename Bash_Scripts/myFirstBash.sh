fbb79b7c798fc39b5fd3209f334eb0834a7e71b5
Hawa Djikine <djikineh@gmail.com>
2016-11-20 01:29:40 -0500

    an update to test2.txt

---
+++ Bash_Scripts/myFirstBash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo
+ echo  "This is the information provided by myFirstBash. Program starts now."
+echo
+echo "Hello $USER"
+echo
+pwd
+ 
+
---
+++ simplevar.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+#A simple variable example
+
+	myvariable=Hello
+	
+	anothervar=Abba
+
+	echo $myvariable $anothervar
+	echo
+ 
+
+
